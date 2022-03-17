ca907b7ea9b633030c3052675f23b2992c253377
gambimo <96704851+gambimo@users.noreply.github.com>
2022-03-17 10:49:21 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,20 @@
-    //메뉴네비게이션 
-    $("nav>ul>li").mouseenter(function () { 
-        $(".submenu").stop().slideDown('slow'); 
-        $(".menu_bg").stop().slideDown('slow'); }); 
-    $("nav>ul>li").mouseleave(function () { 
-        $(".submenu").stop().slideUp('slow'); 
-        $(".menu_bg").stop().slideUp('slow'); });
+$("nav>ul>li").mouseenter(function () { 
+    $(".sub_menu").stop().slideDown('slow'); 
+    $(".menu_bg").stop().slideDown('slow'); 
+}); 
 
-    //좌우롤링배너 
-    start(); 
-    var imgs = $('#slide_img_wrap img').length; 
-    var now = 0; var width = 1200; 
-    function start(){ $('#slide_img_wrap>img') .eq(0).siblings().css('margin-left',-width*now+"px"); 
-    setInterval(function(){slide();},2000); }; 
-    function slide(){ now = now == imgs ? 0 : now += 1; 
-        $('#slide_img_wrap>img:first-child') .eq(now-1).animate({marginLeft:-width*now+"px"},300); 
-        $('#slide_img_wrap>img:first-child') .eq(now-2).animate({marginLeft:-width*now+"px"},300); 
-        $('#slide_img_wrap>img:first-child') .eq(now).css('margin-left','0px'); };
+$("nav>ul>li").mouseleave(function () { 
+    $(".sub_menu").stop().slideUp('slow'); 
+    $(".menu_bg").stop().slideUp('slow'); 
+});
 
-    //탭메뉴 
-    $('#tab_menu>ul>li:first-child').click(function () { 
-        $('.notice').show(); //notice나타나기 
-        $('.gallery').css('display', 'none'); //gallery 숨기기 
+setInterval(function () { 
+    $("#slide_wrap").delay('2500'); 
+    $("#slide_wrap").animate({ marginTop: "-300px" }, '700'); 
+    $("#slide_wrap").delay('2500'); 
+    $("#slide_wrap").animate({ marginTop: "-600px" }, '700'); 
+    $("#slide_wrap").delay('2500'); 
+    $("#slide_wrap").animate({ marginTop: "0" }, '700'); 
+});
 
-    $('#tab_menu>ul>li:first-child>a').css({ 
-        'background': 'white', 'text-decoration': 'underline', 'color' : '#E66B27' }); 
-        
-    $('#tab_menu>ul>li:last-child>a').css({ 
-        'background': 'silver', 'text-decoration': 'none', 'color' : '#333333' }); }); 
-        
-    $('#tab_menu>ul>li:last-child').click(function () { 
-        $('.notice').css('display', 'none'); 
-        $('.gallery').show(); 
-        
-        $('#tab_menu>ul>li:last-child>a').css({ 
-            'background': 'white', 'text-decoration': 'underline', 'color' : '#E66B27' }); 
-        $('#tab_menu>ul>li:first-child>a').css({ 
-            'background': 'silver', 'text-decoration': 'none', 'color' : '#333333' }); 
-    });
+alert("hello javascript");
